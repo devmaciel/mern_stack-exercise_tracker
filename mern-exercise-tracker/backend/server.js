c963ff4e486e7591d601230d1cdcc7f832b3fb54
João Pedro Maciel <joomaciiel2@outlook.com>
2020-09-04 17:33:09 -0300

    initial server configs

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,28 @@
+//Dependencies
+const express = require('express');
+const cors = require('cors');
+const moongose = require('mongoose');
+
+//ENV Config
+require('dotenv').config();
+
+//Express Server
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middlewares 
+app.use(cors());
+app.use(express.json());
+
+//Moongose Connection
+const uri = process.env.ATLAS_URI;
+moongose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = moongose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+//Listen Port Start Server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
