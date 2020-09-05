413496a8261746130c57cbd03f6c1a3efa8ebc3b
João Pedro Maciel <joomaciiel2@outlook.com>
2020-09-05 17:39:29 -0300

    create user component

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 
 export default class CreateUsers extends Component {
+
+    //Constructor
+    constructor(props){
+        super(props);
+
+        //Binds (old version sintax, newers version have other sintax with autobinding) 
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        //States
+        this.state = {
+            username: ''
+        }
+    }
+
+
+    //Methods Setter
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    //My Methods
+    onSubmit(e){
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username
+        }
+
+        console.log(user);
+
+        this.setState({
+            username: ''
+        });
+
+    }
+
+
+    //Render
     render(){
         return(
             <div>
-                <p>You are on the Create Users List component!</p>
+               <h3>Create New User</h3>
+               <form onSubmit={this.onSubmit}>
+
+                    <div className="form-group">
+                        <label htmlFor="inputUsername">Username: </label>
+                        <input type="text" name="inputUsername" required className="form-control" value={this.state.username} onChange={this.onChangeUsername} />
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+
+               </form> 
             </div>
         )
     }
